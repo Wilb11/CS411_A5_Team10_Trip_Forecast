9bf42d6e7c7bb97dbb5f02cfbdcf6bf62b3d8a60
marilynrisinger1 <marilynr@bu.edu>
2023-05-04 12:27:18 -0400

    finished the api file but need to actually implement it to work

--- main/trip_forecaster/lib/flightApi.dart
+++ main/trip_forecaster/lib/flightApi.dart
@@ -1,17 +1,22 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-class ApiService {
-  static const String baseUrl = 'https://api.example.com';
+class FlightApi {
+  final String baseUrl = 'https://partners.api.skyscanner.net/apiservices';
 
-  Future<Map<String, dynamic>> fetchData() async {
-    final response = await http.get(Uri.parse('$baseUrl/data'));
+  Future<Map<String, dynamic>> searchFlights(String origin, String destination, String departureDate) async {
+    final response = await http.get(
+      Uri.parse('$baseUrl/browsequotes/v1.0/US/USD/en-US/$origin/$destination/$departureDate'),
+      headers: {
+        'Accept': 'application/json',
+      },
+    );
+
     if (response.statusCode == 200) {
       return jsonDecode(response.body);
     } else {
-      throw Exception('Failed to fetch data');
+      throw Exception('Failed to load flight data');
     }
   }
-
-  // Add more methods for other API calls as needed
 }
+
